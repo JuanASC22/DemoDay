7e28a76605deb409281a867bdff753c47257dcb0
Juan Davila <jdavilaortiz7@gmail.com>
2022-08-20 16:18:50 -0400

    submit commit

---
+++ data.js
@@ -0,0 +1,156 @@
+let stuff =  {
+    "request": {
+        "keywords": "elementary math",
+        "lang": "en",
+        "provider": "Amazon",
+        "sort": "popularity",
+        "max": 3,
+        "model": "strict",
+        "address": "",
+        "distanceMax": "",
+        "ageMin": "",
+        "ageMax": "",
+        "popularityMin": 0,
+        "levelMin": "",
+        "levelMax": "",
+        "learningTimeMin": "",
+        "learningTimeMax": "",
+        "publisher": "",
+        "author": "",
+        "free": "",
+        "page": 0,
+        "id": "7ffab4bf-a5bc-48e2-9350-2aadc8b36dbe",
+        "match": "best-effort"
+    },
+    "response": {
+        "content": 
+[
+{
+                "title": "McGraw-Hill My Math, Grade 2, Student Edition, Volume 1 (ELEMENTARY MATH CONNECTS)",
+                "url": "https://www.amazon.com/dp/0021150214",
+                "description": "This set provides the consumable Student Edition, Volume 1, which contains everything students need to build conceptual understanding, application, and procedural skill and fluency with math content organized to address CCSS. Students engage in learning with write-in text on vocabulary support and h...",
+                "picture": "https://m.media-amazon.com/images/I/91S71HOsVuL._AC_UY654_FMwebp_QL65_.jpg",
+                "provider": "Amazon",
+                "bloom": [
+                    "Understand"
+                ],
+                "type": [
+                    "Book"
+                ],
+                "publisher": [
+                    {
+                        "name": "McGraw-Hill Education"
+                    }
+                ],
+                "author": [
+                    {
+                        "name": "ALTIERI"
+                    },
+                    {
+                        "name": "McGraw-Hill Education"
+                    }
+                ],
+                "level": -1,
+                "lang": "en",
+                "learningTimeValue": 8,
+                "learningTimeUnit": "h",
+                "PriceSpecification": {
+                    "@context": "https://schema.org/",
+                    "@type": "PriceSpecification",
+                    "price": 4,
+                    "priceCurrency": "$",
+                    "price-free": false
+                }
+},
+{
+                "title": "My Math, Grade 3, Vol. 1 (ELEMENTARY MATH CONNECTS)",
+                "url": "https://www.amazon.com/dp/0021150222",
+                "description": "This set provides the consumable Student Edition, Volume 1, which contains everything students need to build conceptual understanding, application, and procedural skill and fluency with math content organized to address CCSS. Students engage in learning with write-in text on vocabulary support and h...",
+                "picture": "https://m.media-amazon.com/images/I/91krM-DGzTL._AC_UY654_FMwebp_QL65_.jpg",
+                "provider": "Amazon",
+                "bloom": [
+                    "Understand"
+                ],
+                "type": [
+                    "Book"
+                ],
+                "publisher": [
+                    {
+                        "name": "McGraw-Hill Education; 1st edition (July 7"
+                    }
+                ],
+                "author": [
+                    {
+                        "name": "McGraw-Hill Education"
+                    }
+                ],
+                "level": -1,
+                "lang": "en",
+                "learningTimeValue": 11,
+                "learningTimeUnit": "h",
+                "PriceSpecification": {
+                    "@context": "https://schema.org/",
+                    "@type": "PriceSpecification",
+                    "price": 5,
+                    "priceCurrency": "$",
+                    "price-free": false
+                }
+},
+{
+                "title": "McGraw-Hill My Math, Grade 1, Student Edition, Volume 1 (ELEMENTARY MATH CONNECTS)",
+                "url": "https://www.amazon.com/dp/0021150206",
+                "description": "This set provides the consumable Student Edition, Volume 1, which contains everything students need to build conceptual understanding, application, and procedural skill and fluency with math content organized to address CCSS. Students engage in learning with write-in text on vocabulary support and h...",
+                "picture": "https://m.media-amazon.com/images/I/91yp7Zo96aL._AC_UY654_FMwebp_QL65_.jpg",
+                "provider": "Amazon",
+                "bloom": [
+                    "Understand"
+                ],
+                "type": [
+                    "Book"
+                ],
+                "publisher": [
+                    {
+                        "name": "McGraw-Hill Education; 1st edition (July 8"
+                    }
+                ],
+                "author": [
+                    {
+                        "name": "McGraw-Hill Education"
+                    }
+                ],
+                "level": -1,
+                "lang": "en",
+                "learningTimeValue": 10,
+                "learningTimeUnit": "h",
+                "PriceSpecification": {
+                    "@context": "https://schema.org/",
+                    "@type": "PriceSpecification",
+                    "price": 2,
+                    "priceCurrency": "$",
+                    "price-free": false
+                }
+}
+],
+        "metadata": {
+            "page": 0,
+            "number item per page": 3,
+            "total number item": 13,
+            "hasMore": true
+        },
+        "msg": "A00 - These results are our best effort to answer your query."
+    }
+} 
+console.log(stuff)
+
+    // let result = Math.floor(Math.random() * 3);
+    // console.log(result);
+
+// My attempt to transfer the info to the description html
+// document.querySelector(".books")
+// document.querySelector(".name")
+
+// document.booksDesc.appendChild(".books")
+// document.nameDesc.appendChild(".name")
+
+// const imgN = myJson.response.content[result].picture
+// const nameN = myJson.response.content[result].title
